c5566d63afe9071ba27cb5cd399eeb6a20952fb8
Xing Li <msuli_1120@ymail.com>
2017-04-17 20:14:51 -0700

    fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -28,7 +28,7 @@ gulp.task('jsBrowserify', ['concatInterface'],function(){
 });
 
 gulp.task('concatInterface', function () {
-  return gulp.src(['./js/pingpong-interface.js', './js/signup-interface.js'])
+  return gulp.src(['./js/*-interface.js'])
   .pipe(concat('allConcat.js'))
   .pipe(gulp.dest('./tmp'));
 });
